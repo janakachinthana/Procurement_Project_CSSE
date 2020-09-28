ffb8c71e23ce429357c4a5da5bccba8ce098e458
janakachinthana <janakachinthana1@gmail.com>
2020-09-29 01:41:25 +0530

    login component created and link without content of the login page

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -13,7 +13,7 @@ const routes: Routes =[
     redirectTo: 'dashboard',
     pathMatch: 'full',
   }, {
-    path: '',
+    path: 'main',
     component: AdminLayoutComponent,
     children: [{
       path: '',
