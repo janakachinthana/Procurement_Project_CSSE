aef00e5df2788623097f05bbbcf534a79f863509
janakachinthana <janakachinthana1@gmail.com>
2020-10-04 22:54:26 +0530

    model created to employee

---
+++ src/app/models/employee.model.ts
@@ -0,0 +1,12 @@
+export class Employee {
+
+    company: string;
+    userName: string;
+    email: string;
+    firstName: string;
+    lastName: string;
+    projectName: string;
+    address: string;
+    proffession: string;
+
+}
