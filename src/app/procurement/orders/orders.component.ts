1d0174f681d64931bf9fbf4e64c3b7b121b3b230
janakachinthana <janakachinthana1@gmail.com>
2020-10-16 13:43:23 +0530

    Add button to approve orders in procument

--- src/app/procurement/orders/orders.component.ts
+++ src/app/procurement/orders/orders.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { NewOrder } from 'app/common-component/shared/models/new-order.model';
+import { OrderNowService } from 'app/common-component/shared/services/order-now.service';
 
 @Component({
   selector: 'app-orders',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdersComponent implements OnInit {
 
-  constructor() { }
+  list : NewOrder[];
+
+  constructor(
+    private service : OrderNowService,
+    private fireStore: AngularFirestore
+  ) { }
 
   ngOnInit(): void {
+
+    this.service.getNewOrders().subscribe(actionArray=>{
+      this.list = actionArray.map(item => {
+        return {   
+          id: item.payload.doc.id, ...item.payload.doc.data() as NewOrder}
+      })
+    });
+  
   }
 
+  addToApprovedList(){
+    this.list.forEach(item => {
+      this.fireStore.collection('approved').doc(item.id.toString()).set(item);  
+      // this.fireStore.collection('new-orders').doc(item.id.toString()).delete();
+    });
+}
+  
+  
 }
