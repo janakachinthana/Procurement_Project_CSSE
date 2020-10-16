d18eaac9be7e0f014b9f3ecc926268b82a23cd07
janakachinthana <janakachinthana1@gmail.com>
2020-10-16 16:06:37 +0530

    comments added to component module class

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,25 +1,32 @@
+// import statements
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-
 import { FooterComponent } from './footer/footer.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 
+// ngModule imports
 @NgModule({
   imports: [
     CommonModule,
     RouterModule,
   ],
+
+  // component declareration
   declarations: [
     FooterComponent,
     NavbarComponent,
     SidebarComponent
   ],
+
+  // compoent exports
   exports: [
     FooterComponent,
     NavbarComponent,
     SidebarComponent
   ]
 })
+
+// lass declareration
 export class ComponentsModule { }
