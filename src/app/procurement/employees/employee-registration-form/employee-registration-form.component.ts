b1d924a4cf6177a0654352024e70fe11e6b964e8
janakachinthana <janakachinthana1@gmail.com>
2020-10-16 17:27:16 +0530

    added comments for employee register component

--- src/app/procurement/employees/employee-registration-form/employee-registration-form.component.ts
+++ src/app/procurement/employees/employee-registration-form/employee-registration-form.component.ts
@@ -1,67 +1,67 @@
+// import statement
 import { Component, Inject, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { NgForm } from '@angular/forms';
-import { MatDialog, MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { Employee } from 'app/common-component/shared/models/employee.model';
 import { EmployeeService } from 'app/common-component/shared/services/employee.service';
-import { ToastrService } from 'ngx-toastr/toastr/toastr.service';
 
+// components declareration
 @Component({
   selector: 'app-employee-registration-form',
   templateUrl: './employee-registration-form.component.html',
   styleUrls: ['./employee-registration-form.component.css']
 })
+
+// class declareration 
 export class EmployeeRegistrationFormComponent implements OnInit {
 
+  // implementation of constructor
   constructor(
-    private service: EmployeeService,
-    private fireStore : AngularFirestore,
-    private _snackBar: MatSnackBar) { }
+    private service: EmployeeService, // employee service class declareration
+    private fireStore: AngularFirestore, // AngularFirestore class declareration
+    private _snackBar: MatSnackBar // MatSnackBar class declareration
+  ) { }
 
+
+  // initializing method implementation
   ngOnInit(): void {
-    this.resetForm();
+    this.resetForm(); // call the reset function
   }
 
-
+  // implementation of reset form values
   resetForm(form?: NgForm) {
-    if(form != null)
+    if (form != null) 
       form.resetForm();
     this.service.formData = {
-      EmployeeID : null,
-      FullName : '',
-      Designation : '',
-      projectName : '',
-      SiteAddress : '',
-      Nic : '',
-      Contact : null,
-      Email : '',
-      Password : 'ABCabc123',
+      EmployeeID: null,
+      FullName: '',
+      Designation: '',
+      projectName: '',
+      SiteAddress: '',
+      Nic: '',
+      Contact: null,
+      Email: '',
+      Password: 'ABCabc123',
 
     }
   }
 
-  onSubmit(form:NgForm){
-    let data =Object.assign({},form.value);
-  
-  
-      // this.fireStore.collection('new-order').add(data); 
-      this.fireStore.collection('Employees').doc(this.service.formData.Contact.toString()).set(data);   
-      this.openSnackBar();
-      this.resetForm(form);
-    
-   
-    this.resetForm(form);
+//  method of submit button implementation 
+  onSubmit(form: NgForm) {
+    let data = Object.assign({}, form.value); // assign form data to vaariable
+    this.fireStore.collection('Employees').doc(this.service.formData.Contact.toString()).set(data); //insert the data into the database
+    this.openSnackBar(); // show notification 
+    this.resetForm(form); // calling method to reset form
   }
 
-
+  // notification method for insertion
   openSnackBar() {
     this._snackBar.open('Item added successfully', 'Done!', {
       duration: 2000,
     });
   }
 
-  
+// notification method for update
   openSnackBarUpdate() {
     this._snackBar.open('Item Updated successfully', 'Done!', {
       duration: 2000,
